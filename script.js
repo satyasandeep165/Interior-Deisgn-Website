21daecf49f65d051c1240e41858b6d457ae706ed
satyasandeep165 <88207329+satyasandeep165@users.noreply.github.com>
2022-03-09 11:43:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
 
+
 document.querySelector(".scroll-btn").addEventListener("click", () => {
   document.querySelector("html").style.scrollBehavior = "smooth";
   setTimeout(() => {
